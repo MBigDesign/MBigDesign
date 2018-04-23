d2136e53e80e2e23e14f46a8771896ff4c6ffcf0
20721900 <20721900@sun.ac.za>
2018-04-23 02:14:25 +0200

    Uploading initial project

---
+++ mbigdesign/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:mbigdesign/ui/home/HomePage.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "Library Ways",
+    home: new HomePage(),
+  ));
+}
