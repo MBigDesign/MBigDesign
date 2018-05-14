c89149d46f3b09b60ef22e30ae8c136d87563b90
20721900 <20721900@sun.ac.za>
2018-05-14 09:44:38 +0200

    renamed project

--- mbigdesign/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:mbigdesign/ui/home/HomePage.dart';
-
-void main() {
-  runApp(new MaterialApp(
-    title: "Library Ways",
-    home: new HomePage(),
-  ));
-}
